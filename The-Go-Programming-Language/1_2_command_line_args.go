53227777f2d4350012b449edee760fe71be06ef2
isoppp <prog@d2deck.com>
2016-10-06 19:04:18 +0900

    Modify don't need semicolons

--- The-Go-Programming-Language/1_2_command_line_args.go
+++ The-Go-Programming-Language/1_2_command_line_args.go
@@ -8,8 +8,8 @@ import (
 func main() {
     var s, sep string
     for i := 1; i < len(os.Args); i++ {
-        s += sep + os.Args[i];
+        s += sep + os.Args[i]
         sep = " "
     }
-    fmt.Println(s);
+    fmt.Println(s)
 }
--- The-Go-Programming-Language/1_2_command_line_args2.go
+++ The-Go-Programming-Language/1_2_command_line_args2.go
@@ -11,5 +11,5 @@ func main() {
         s += sep + arg
         sep = " "
     }
-    fmt.Println(s);
+    fmt.Println(s)
 }
